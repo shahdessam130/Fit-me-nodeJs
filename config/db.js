77d75ad125713cc924ad1fd91cc4c22200c8445a
shahdessam130 <159285722+shahdessam130@users.noreply.github.com>
2024-06-24 22:51:15 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,8 +4,7 @@ require('dotenv').config(); // لتحميل متغيرات البيئة من م�
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true, 
-            useUnifiedTopology: true 
+            
         });
         console.log('MongoDB connected');
     } catch (err) {
