ff2be2d910377ac03f9667092183ecb6aa3addc9
shahdessam130 <159285722+shahdessam130@users.noreply.github.com>
2024-06-27 14:34:59 +0300

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -8,7 +8,6 @@ const auth = (req, res, next) => {
     if (!token) {
         return res.status(401).json({ msg: 'No token, authorization denied' });
     }
-
     try {
         const decoded = jwt.verify(token, jwtSecret);
         req.user = decoded.user;
