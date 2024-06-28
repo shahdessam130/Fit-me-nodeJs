ddb27590515ca8b7c17d27c15598ae813657bfa2
shahdessam130 <159285722+shahdessam130@users.noreply.github.com>
2024-06-29 02:47:42 +0300

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,13 +1,13 @@
-
 const jwt = require('jsonwebtoken');
 const jwtSecret = process.env.JWT_SECRET;
 
 const auth = (req, res, next) => {
-    const token = req.header('x-auth-token');
+    const token = req.header('X-Authorization');
 
     if (!token) {
         return res.status(401).json({ msg: 'No token, authorization denied' });
     }
+
     try {
         const decoded = jwt.verify(token, jwtSecret);
         req.user = decoded.user;
