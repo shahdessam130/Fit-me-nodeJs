f612c6b588491f8918a2dd7dd6e263c880caa345
shahdessam130 <159285722+shahdessam130@users.noreply.github.com>
2024-06-29 01:26:19 +0300

    Update user.js

--- models/user.js
+++ models/user.js
@@ -35,19 +35,5 @@ const UserSchema = new mongoose.Schema({
     }]
 });
 
-// Password hashing middleware
-UserSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) {
-        return next();
-    }
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-});
-
-// Password verification method
-UserSchema.methods.comparePassword = function (candidatePassword) {
-    return bcrypt.compare(candidatePassword, this.password);
-};
 
 module.exports = mongoose.model('User', UserSchema);
